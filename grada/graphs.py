5ee812784c8f5afa84243a91279a813898564e66
lmarchetti02 <00lucamarchetti00@gmail.com>
2023-09-05 17:59:56 +0200

    tolto __new__

--- grada/graphs.py
+++ grada/graphs.py
@@ -441,16 +441,16 @@ class Canvas:
 
     """
 
-    __instance = None
+    # __instance = None
 
-    def __new__(cls, *args, **kwargs):
-        if cls.__instance is None:
-            cls.__instance = super().__new__(cls)
-        else:
-            print("Esiste già un oggetto Canvas.")
-            plt.close()
+    # def __new__(cls, *args, **kwargs):
+    #     if cls.__instance is None:
+    #         cls.__instance = super().__new__(cls)
+    #     else:
+    #         print("Esiste già un oggetto Canvas.")
+    #         plt.close()
 
-        return cls.__instance
+    #     return cls.__instance
 
     def __init__(
         self,
