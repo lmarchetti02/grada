04389f8a19830524eea2cb3ca1aff3ba615c6209
lmarchetti02 <00lucamarchetti00@gmail.com>
2023-09-10 20:33:01 +0200

    added blueprint

--- grada/__init__.py
+++ grada/__init__.py
@@ -17,10 +17,9 @@ except Exception as _:
     print("La directory '/text' esiste già.")
 
 # creates the text file
-with open("text/text_example.txt", "w") as file:
-    file.write(
-        r"""
-FILE DI TESTO CON LE STRINGHE DA MOSTRARE NEL GRAFICO. 
+with open("text/text_example.txt", "w") as f:
+    f.write(
+        r"""FILE DI TESTO CON LE STRINGHE DA MOSTRARE NEL GRAFICO. 
 NON MODIFICARE L'ORDINE DELLE RIGHE, NÈ AGGIUNGERE SPAZI.
 LE PARTI COMPRESE TRA "$ $" SONO DA SCRIVERE IN LATEX.
 #########################################################
@@ -40,5 +39,19 @@ Nome dati 2:
 
 Nome funzione 1: 
 Nome funzione 2: 
+        """
+    )
+
+with open("text/blueprint.txt", "w") as f:
+    f.write(
+        r"""text = g.Text("text/text.txt")
+
+canvas = g.Canvas(text, save="test")
+
+g.ScatterPlot().draw(canvas, text, x, y, y_err)
+
+g.Plot().draw(canvas, text, x, lambda x: x, 5)
+
+canvas.mainloop(show=True)
 """
     )
